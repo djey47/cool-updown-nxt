548e600869a092607245e9e57cc87a495ad1e752
djey47 <djey47@github.com>
2023-11-06 14:16:14 +0100

    feature(api/helpers): improve logs

--- api/src/helpers/recaller.ts
+++ api/src/helpers/recaller.ts
@@ -2,7 +2,7 @@
  * Just a wrapper for NodeJS 'setTimeout' function
  */
 export function recall(which: () => void, inMs: number, catcher?: (ref: NodeJS.Timeout) => void) {
-  console.log('recaller::recall Will recall in milliseconds:', inMs);
+  console.log(`recaller::recall Will recall '${which.name}' in milliseconds:`, inMs);
 
   const timeout = setTimeout(which, inMs);
   if (catcher) {
