ee88e16654f0b2caa41d10fa45b6a4b83609490b
djey47 <djey47@github.com>
2023-11-13 22:12:24 +0100

    chore(web): configure API proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: getProxyConfiguration('/config', '/power-on', '/diags'),
+  },
+});
+
+function getProxyConfiguration(...paths: string[]) {
+  return paths.reduce((proxyConfiguration, p) => {
+    proxyConfiguration[p] = {
+      target: 'http://localhost:3000',
+      changeOrigin: true,
+      secure: false,
+      ws: true,
+    };
+    return proxyConfiguration;
+  }, {});
+}
