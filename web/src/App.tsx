68c71fefbff93f24b764214a5d820b54cfd1d0be
djey47 <djey47@github.com>
2023-11-14 11:00:42 +0100

    chore(web): add query client provider to app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import PageContainer from './components/page/PageContainer';
 import Dashboard from './components/page/Dashboard';
 import Banner from './components/molecules/banner/Banner';
 
+// Create a react-query client
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <PageContainer>
-      <Banner />
-      <Dashboard />
-    </PageContainer>
+    <QueryClientProvider client={queryClient}>
+      <PageContainer>
+        <Banner />
+        <Dashboard />
+      </PageContainer>
+    </QueryClientProvider>
   )
 };
 
