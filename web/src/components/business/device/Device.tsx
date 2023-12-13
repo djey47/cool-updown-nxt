e6ac4af04e0e4491d6789e5d6e07074ac29ce06d
djey47 <djey47@github.com>
2023-12-13 09:34:06 +0100

    feature(web/device): change power state and trigger toasts before awaiting actual operation outcome

--- web/src/components/business/device/Device.tsx
+++ web/src/components/business/device/Device.tsx
@@ -22,7 +22,7 @@ interface DeviceProps {
   deviceInfo: DeviceInfo;
 }
 
-type PopupDisplayMode  = 'details' | 'logs' | 'none';
+type PopupDisplayMode = 'details' | 'logs' | 'none';
 
 const Device = ({ deviceInfo }: DeviceProps) => {
   const [popupDisplayMode, setPopupDisplayMode] = useState<PopupDisplayMode>('none');
@@ -30,12 +30,12 @@ const Device = ({ deviceInfo }: DeviceProps) => {
   const [isPerformingPowerOff, setPerformingPowerOff] = useState(false);
 
   const { id: deviceId, network: { hostname: deviceName } } = deviceInfo;
-  const { data: diagsQueryData, isFetching: isFetchingDiags} = useQuery({
+  const { data: diagsQueryData, isFetching: isFetchingDiags } = useQuery({
     queryKey: ['diags', deviceId],
     queryFn: ({ queryKey }) => getDiagnosticsForDevice(queryKey[1]),
     refetchInterval: 5000,
   });
-  const { data: statsQueryData, isFetching: isFetchingStats} = useQuery({
+  const { data: statsQueryData, isFetching: isFetchingStats } = useQuery({
     queryKey: ['stats', deviceId],
     queryFn: ({ queryKey }) => getStatisticsForDevice(queryKey[1]),
     refetchInterval: 5000,
@@ -46,31 +46,33 @@ const Device = ({ deviceInfo }: DeviceProps) => {
   const deviceLabel = `${deviceName} (${deviceId})`;
 
   const handlePowerClick = async () => {
-    const infoCommandMessage = `Command has already been sent to device ${deviceLabel}`;
+    const infoCommandMessage = `Operation is still in progress for ${deviceLabel}`;
     const successCommandMessage = `Command has been sent to device ${deviceLabel}`;
 
+    // We first display toast notifications and update state as some power operations (namely OFF via SSH) don't resolve
+    // until services are closed remote-side.
     if (devicePowerState === 'off') {
-      await postPowerOnForDevice(deviceId);
-
       if (isPerformingPowerOn) {
         showAlert('info', 'Power ON', infoCommandMessage);
       } else {
         showAlert('success', 'Power ON', successCommandMessage);
+
+        setPerformingPowerOn(true);
+        setPerformingPowerOff(false);
+
+        await postPowerOnForDevice(deviceId);
       }
-
-      setPerformingPowerOn(true);  
-      setPerformingPowerOff(false);  
     } else if (devicePowerState === 'on') {
-      await postPowerOffForDevice(deviceId);
-
       if (isPerformingPowerOff) {
         showAlert('info', 'Power OFF', infoCommandMessage);
       } else {
         showAlert('success', 'Power OFF', successCommandMessage);
-      }
 
-      setPerformingPowerOff(true);
-      setPerformingPowerOn(false);
+        setPerformingPowerOff(true);
+        setPerformingPowerOn(false);
+
+        await postPowerOffForDevice(deviceId);
+      }
     }
   };
 
@@ -86,7 +88,7 @@ const Device = ({ deviceInfo }: DeviceProps) => {
     }
     if (isPerformingPowerOff && devicePowerState === 'off') {
       setPerformingPowerOff(false);
-    }  
+    }
   }
 
   handleEndedPowerOps();
@@ -95,11 +97,11 @@ const Device = ({ deviceInfo }: DeviceProps) => {
 
   const menuItems: DropDownMenuItem[] = [{
     label: 'Details',
-    clickHandler: handleShowPopup('details') ,
+    clickHandler: handleShowPopup('details'),
     linkHref: '#',
   }, {
     label: 'Logs',
-    clickHandler: handleShowPopup('logs') ,
+    clickHandler: handleShowPopup('logs'),
     linkHref: '#',
   }];
 
