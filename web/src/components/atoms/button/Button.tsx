b1592f03aa8bd4038e9784775ae17e7aeb624d8a
djey47 <djey47@github.com>
2024-01-08 10:51:10 +0100

    feature(web/atoms): add isClickable prop to Button

--- web/src/components/atoms/button/Button.tsx
+++ web/src/components/atoms/button/Button.tsx
@@ -1,15 +1,27 @@
+import classNames from 'classnames';
 import React, { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   children?: React.ReactNode;
+  isClickable?: boolean;
 }
 
-const Button = ({ className = '', children, ...buttonAttributes }: ButtonProps) => {
+const Button = ({ className = '', children, isClickable = true, ...buttonAttributes }: ButtonProps) => {
+  const buttonClassNames = classNames(
+    className,
+    'rounded',
+    'p-2',
+    {
+      'hover:bg-indigo-300/50': isClickable,
+      'cursor-pointer': isClickable,
+      'cursor-default': !isClickable,      
+    },
+  );
   return (
     <button
       type="button"
-      className={`hover:bg-indigo-300/50 rounded p-2 ${className}`}
+      className={buttonClassNames}
       {...buttonAttributes}
     >
       {children}
